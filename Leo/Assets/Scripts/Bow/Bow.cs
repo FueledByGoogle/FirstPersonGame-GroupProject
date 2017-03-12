762ab17dd4ced209df9e79bdeb605cf62ab4e11b
Leo <leo.l.huang@hotmail.com>
2017-03-12 11:06:28 -0700

    Leo Unity Files

---
+++ Leo/Assets/Scripts/Bow/Bow.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bow : MonoBehaviour {
+
+    [Range(0.0f, 1.0f)]
+    public float factor;
+
+    Vector3 firstPosition;
+    Vector3 lastPosition;
+
+    public bool stretching;
+    public bool releasing;
+	public bool bowReady;
+
+    public const float stretchSpeed = 1f;
+    public const float releaseSpeed = 5f;
+
+	public Rigidbody arrow;
+	public const float arrowVelocity = 30f;
+	public Transform arrowSpawnLocation;
+
+    void Start () {
+        firstPosition = transform.localPosition;
+        lastPosition = transform.localPosition + Vector3.up * 0.45f;
+		bowReady = false;
+    }
+
+	void Update () {
+
+        if (stretching) {
+            factor += stretchSpeed * Time.deltaTime;
+
+            if (factor > 1.0f) {
+                factor = 1.0f;
+				bowReady = true;
+            }
+        }
+
+		if (releasing) {
+            factor -= releaseSpeed* Time.deltaTime;
+
+            if (factor < 0.0f) {
+                factor = 0.0f;
+				bowReady = false;
+            }
+        }
+        transform.localPosition = Vector3.Lerp(firstPosition, lastPosition, factor);
+    }
+
+    public void Stretch () {
+        stretching = true;
+        releasing = false;
+    }
+
+    public void Release () {
+        releasing = true;
+        stretching = false;
+    }
+
+	public Rigidbody DrawArrow (){
+		Rigidbody arrowFired = Instantiate (arrow, arrowSpawnLocation.position, arrowSpawnLocation.rotation) as Rigidbody;
+		return arrowFired;
+	}
+
+	public Vector3 BowPosition () {
+		return arrowSpawnLocation.position;
+	}
+
+	//TODO: this won't be needed after orientation of bow is fixed
+	public Quaternion BowRotation () {
+		return arrowSpawnLocation.rotation;
+	}
+
+	public void Fire() {
+		Rigidbody arrowFired = Instantiate (arrow, arrowSpawnLocation.position, arrowSpawnLocation.rotation) as Rigidbody;
+		arrowFired.velocity = arrowVelocity * arrowSpawnLocation.forward * -1;
+	}
+}
---
+++ Leo/Assets/Scripts/Character.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+	public const float maxHealth = 10f;
+	public float health;
+
+	public const float walkSpeed = 1.5f;
+	public const float runSpeed = 5f;
+	public const float turnSpeed = 120f;
+
+	public HealthBar healthBar;	
+	public Rigidbody characterRigidBody;
+
+	void Start () {
+		characterRigidBody = GetComponent<Rigidbody> ();
+		health = maxHealth;
+	}
+	
+
+	void Update () {
+		
+	}
+
+	public void Move (float direction, bool run) {
+
+		if (run == false) {
+			transform.Translate(Vector3.forward * direction * walkSpeed * Time.deltaTime);
+		} else {
+			transform.Translate(Vector3.forward *direction * runSpeed * Time.deltaTime);
+		}
+	}
+
+	public void Rotate (bool turnRight){
+		
+		if (turnRight == true) {
+			transform.Rotate (0, turnSpeed * Time.deltaTime, 0);
+		} else {
+			transform.Rotate (0, (-1) * turnSpeed * Time.deltaTime, 0);
+		}
+
+	}
+
+	public void takeDamage (float damage){
+		health -= damage;
+		if (health < 0) {
+			health = 0;
+		}
+		healthBar.SetHealth (health, maxHealth);
+	}
+}
