7cb2e13ab29a82de2b96f4dd03bef5effeca436d
TeamIcycle <patersontwilight@gmail.com>
2017-03-19 11:12:48 -0700

    Add files via upload

---
+++ ArrowPretend.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowPretend : MonoBehaviour {
+
+	public float lifeTime = 3f;
+	public float damage = 20;
+	public GameObject holder;
+	private Rigidbody theBody;
+
+	float startTime;
+
+	void Start () {
+		startTime = Time.time;
+		theBody = GetComponent<Rigidbody> ();
+	}
+
+	void Update () {
+		
+		if (Time.time > startTime + lifeTime) {
+			Destroy (gameObject);
+		}
+		else{
+			theBody.velocity = transform.forward * 5f * (-1);
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		GameObject player = other.GetComponent<GameObject> ();
+
+		if (player != null) {
+			//player.ApplyDamage (damage);
+			Destroy (gameObject);
+		}
+		
+		if (other.gameObject.CompareTag("Player")){
+			Destroy (gameObject);
+		}
+
+	}
+}
---
+++ Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+
+	public const float walkSpeed = 5f;
+	public const float turnSpeed = 25f;
+
+	public Rigidbody characterRigidBody;
+
+	void Start () {
+		characterRigidBody = GetComponent<Rigidbody> ();
+
+	}
+	
+
+	void Update () {
+		
+		
+		if (Input.GetKey (KeyCode.W)) {
+			characterRigidBody.transform.position -= transform.right * Time.deltaTime * walkSpeed;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			characterRigidBody.transform.Rotate(0f, turnSpeed * Time.deltaTime, 0f);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			characterRigidBody.transform.position += transform.right * Time.deltaTime * walkSpeed;
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			characterRigidBody.transform.Rotate(0f, turnSpeed * Time.deltaTime * (-1), 0f);
+		}
+		
+		
+	}
+}
