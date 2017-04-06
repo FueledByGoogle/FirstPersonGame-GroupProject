f1face63edec9acb84533053072904ff002dd9cc
TeamIcycle <patersontwilight@gmail.com>
2017-04-06 14:56:44 -0700

    Got the signpost working, but...
    everynow and then the signpost either appears too far or to off of the
    sign. If it's too far, then it moves with the player. Signs will need to
    be added to explain the combat system

--- 205 Project/Assets/Scripts/Nicola's Scripts/SignPostScript.cs
+++ 205 Project/Assets/Scripts/Nicola's Scripts/SignPostScript.cs
@@ -4,77 +4,32 @@ using UnityEngine;
 
 public class SignPostScript : MonoBehaviour {
 	
-	public GameObject backgroundPanel;
-	
-	public GameObject buttonPanel;
-	public UnityEngine.UI.Text pressR;
-	
-	PlayerController player;
-	
-	//public int maxTextBoxes; //incase I can figure out how to run through dialogue options
-	
-	public UnityEngine.UI.Text reading1;
-	
-	bool isReadingThis;
-	int numberOfTextBoxes;
-	Vector3 origin;
+	public GameObject enterText;
 
 
 	// Use this for initialization
-	void Start () {
-		isReadingThis = false;
-		numberOfTextBoxes = 0;
-		backgroundPanel.SetActive(false);
-		pressR.enabled = false;
-		reading1.enabled = false;
-		buttonPanel.SetActive(false);
+	void Start ()
+	{
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(isReadingThis == true){
-			player.transform.position = origin;
-			if(numberOfTextBoxes == 0){
-				buttonPanel.SetActive(false);
-				backgroundPanel.SetActive(true);
-				reading1.enabled = true;
-				numberOfTextBoxes++;
-			}
-			else if(Input.GetKeyDown (KeyCode.R) && numberOfTextBoxes == 1){
-				reading1.enabled = false;
-				isReadingThis = false;
-				numberOfTextBoxes = 0;
-				pressR.enabled = false;
-				backgroundPanel.SetActive(false);
-			}
-		}
-		
-		if (Input.GetKeyDown (KeyCode.R) && pressR.enabled == true) { 
-			pressR.enabled = false;
-			isReadingThis = true;
+
+	void Update ()
+	{
+
+
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.transform.root.CompareTag ("Player")) {
+			enterText.SetActive (true);
 		}
 	}
-	
-	void OnTriggerEnter (Collider coll) {
-		player = coll.gameObject.transform.root.GetComponent<PlayerController > ();
-		//The following if statement enables the button prompt once in the trigger range of a sign
-		if (coll.gameObject.transform.root.CompareTag("Player")){
-			
-			buttonPanel.SetActive(true);
-			pressR.enabled = true;
-			origin = player.transform.position;
+
+	void OnTriggerExit (Collider other)
+	{
+		if (other.gameObject.transform.root.CompareTag ("Player")) {
+			enterText.SetActive (false);
 		}
-		
 	}
-	
-	void OnTriggerExit (Collider coll) {
-		
-		//The following if statement DISABLES the button prompt once leaving the trigger of a sign
-		if (coll.gameObject.transform.root.CompareTag("Player")){
-			pressR.enabled = false;
-			buttonPanel.SetActive(false);
-			//player = null;
-		}
-		
-	}
+
 }
