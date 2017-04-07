84c13a7768512b20ed39728e3b41613e29e1de91
Yuki Hayashi <yuki_hayashi@hotmail.com>
2017-04-07 10:04:28 -0700

    added basic floor generator room

---
+++ 205 Project/Assets/Scripts/Yuki's Scripts/fallDamage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallDamage : MonoBehaviour {
+
+	public GameObject spawnPoint;
+	public float damage;
+	// Use this for initialization
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.gameObject.transform.root.CompareTag ("Player")) {
+			col.transform = spawnPoint.transform;
+			GameObject player = find
+			Character fallDamage = player.transform.root.GetComponent<Character> ();
+			fallDamage.TakeDamage (damage);
+			player.transform.position = spawnPoint.transform.position;
+		}
+	}
+}
