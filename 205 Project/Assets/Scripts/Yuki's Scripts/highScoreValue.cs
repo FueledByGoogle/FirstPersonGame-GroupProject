454628cceafa5541bd6ffcc3551d5a81b93931b4
Yuki Hayashi <yuki_hayashi@hotmail.com>
2017-04-09 15:38:06 -0700

    fixed high score

---
+++ 205 Project/Assets/Scripts/Yuki's Scripts/highScoreValue.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class highScoreValue : MonoBehaviour {
+
+	public Text score;
+	// Use this for initialization
+	void Start () {
+		score.text = ("Highscore: " + PlayerPrefs.GetInt ("highscore", 0));
+	}
+
+}
