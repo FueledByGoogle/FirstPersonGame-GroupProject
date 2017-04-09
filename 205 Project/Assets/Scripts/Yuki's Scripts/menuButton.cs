1be83849117b7ec79f19f6edf95ea82a6f97d1ac
Yuki Hayashi <yuki_hayashi@hotmail.com>
2017-04-09 15:27:01 -0700

    added main menu

---
+++ 205 Project/Assets/Scripts/Yuki's Scripts/menuButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class menuButton : MonoBehaviour {
+
+	public void LoadScene(int level){
+		if(level == -1){
+			Application.Quit ();
+		}
+		else {
+			SceneManager.LoadScene (level, LoadSceneMode.Single);
+		}
+	}
+		
+}
